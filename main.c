91c98ade6d61ce1615c4d6d36ca84a91dcd6ff44
macharia <mrmachariaorsam@gmail.com>
2026-01-28 10:35:31 +0300

    last commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+//function
+void  HappyBirthday(char name[], int age) {
+  printf("Happy Birthday to You😻😻!\n");
+  printf("Happy Birthday to You🎉🎉!\n");
+  printf("Happy Birthday %s 💕💕💕!\n", name);
+  printf("Happy Birthday to You!🍾🍾🍾\n\n");
+  printf("You are now %d years old 😘😘! Hurrayyyyyyyy\n\n", age);
+  printf("You are special and loved! Wishing you a day filled with happiness and a year filled with joy.🎂🎂🎂\n\n");
+  printf("Enjoy your special day! May all your dreams come true!🎁🎁🎁\n\n");
+  printf("With lots of love, hugs, and kisses! 😘😘😘\n\n");
+  printf("You r getting older and wiser! Keep shining bright! 🌟🌟🌟\n\n");
+  printf("Ensure ushinde Gael sawa🤣🤣🤣!\n\n");
+}
+
+int main() {
+    printf("This is a simple birthday greeting program kwa sis wa gael si wwe gael🤣🤣.\n\n");
+
+    char name[] = "Gael 's lil sis";
+    int age = 7;
+
+    HappyBirthday(name,age);
+HappyBirthday(name,age);
+HappyBirthday(name,age);
+HappyBirthday(name,age);
+HappyBirthday(name,age);
+
+ 
+
+  
+  
+return 0;  
+}
